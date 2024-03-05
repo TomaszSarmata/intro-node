d59abe083ec1ff859b8e11e1f8c2d0bfd50c538a
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-05 09:51:35 +0000

    Added logic to note so that it grabs the 3rd item from argv array and creates an object with that item and then logs it to the terminal

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
-console.log("hello world");
+const note = process.argv[2]; //first two items in the argv arr are the instance of the node and then the js file that gets executed. The third one is the argument that we pass after the command
+
+//Create a data model for a note
+
+const newNote = {
+  content: note,
+  id: Date.now(),
+};
+
+//Check with clg if works fine
+console.log(newNote);
