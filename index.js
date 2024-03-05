feb12f022f37fb63b6769061d0b0687a608dc115
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-05 10:20:11 +0000

    Added some comments to explain difference between imorting different type of modules

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 import { count } from "./utils.js";
 import whateverNameAsItsDefault from "./utils.js";
+import fs from "fs"; //if it's internal module we don't provide the path (as opposed to the user created modules)
+import _ from "lodash"; //this is how we import 3rd party modules that we have installed on the project
 
 const note = process.argv[2]; //first two items in the argv arr are the instance of the node and then the js file that gets executed. The third one is the argument that we pass after the command
 
