a43d995972b3cb1889d32cbb1ee00d6ee1eecc87
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-05 10:12:19 +0000

    Created a simple module and exported it to then import it to the index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+import { count } from "./utils.js";
+
 const note = process.argv[2]; //first two items in the argv arr are the instance of the node and then the js file that gets executed. The third one is the argument that we pass after the command
 
 //Create a data model for a note
@@ -8,6 +10,3 @@ const newNote = {
   content: note,
   id: Date.now(),
 };
-
-//Check with clg if works fine
-console.log(newNote);
