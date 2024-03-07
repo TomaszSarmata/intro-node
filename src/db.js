8fcaa2834ea2c1288560bf237bd79136c7af0962
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-07 11:31:38 +0000

    Created a function to save content to our DB

--- src/db.js
+++ src/db.js
@@ -3,7 +3,15 @@ import { fileURLToPath } from "node:url";
 
 const DB_PATH = fileURLToPath(new URL("../db.json", import.meta.url));
 
+//getting data from the db
 export const getDB = async () => {
   const db = await fs.readFile(DB_PATH, "utf-8");
   return JSON.parse(db);
+};
+
+//saving data into db
+
+export const saveDB = async () => {
+  await fs.writeFile(DB_PATH, JSON.stringify(db, null, 2));
+  return db;
 };
