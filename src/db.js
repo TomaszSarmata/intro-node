7286a10787f939181cbfb713aff305824e8825e4
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-07 11:39:41 +0000

    Create insertDB function to push the new note into our array and return a new note for us

--- src/db.js
+++ src/db.js
@@ -1,3 +1,5 @@
+//Here we are creating all the logic that will sit between our DB and our command.js so that we don't have to write the same code over and over again.
+
 import fs from "node:fs/promises";
 import { fileURLToPath } from "node:url";
 
@@ -11,7 +13,15 @@ export const getDB = async () => {
 
 //saving data into db
 
-export const saveDB = async () => {
+export const saveDB = async (db) => {
   await fs.writeFile(DB_PATH, JSON.stringify(db, null, 2));
   return db;
+};
+
+//appending saved content into the array in our DB
+export const insertDB = async (note) => {
+  const db = await getDB();
+  db.notes.push(note);
+  await saveDB(db);
+  return note;
 };
