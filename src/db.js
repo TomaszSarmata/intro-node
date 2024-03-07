8d7b1192719ee6ae04584db60ba08655bf58371a
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-07 11:22:01 +0000

    Created a function to read the file from our db and return the js object for us

--- src/db.js
+++ src/db.js
@@ -2,3 +2,8 @@ import fs from "node:fs/promises";
 import { fileURLToPath } from "node:url";
 
 const DB_PATH = fileURLToPath(new URL("../db.json", import.meta.url));
+
+export const getDB = async () => {
+  const db = await fs.readFile(DB_PATH, "utf-8");
+  return JSON.parse(db);
+};
